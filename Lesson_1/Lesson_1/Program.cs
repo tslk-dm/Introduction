4749367001741e67187a7ac4d7da48f46a212af7
tslk_dm <tislyukdmitriy@mail.ru>
2020-12-09 16:01:32 +0300

    add a folder with the first homework

---
+++ Lesson_1/Lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Helo, {name}, today is {DateTime.Now}.");
+        }
+    }
+}
